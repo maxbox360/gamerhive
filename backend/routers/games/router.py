bffe49a9e6510a3a0614517e5e9d3fbc400128bc
Max Sanderlin <msanderlin08@gmail.com>
2025-10-05 20:51:11 -0500

    search by genre or platform

--- backend/routers/games/router.py
+++ backend/routers/games/router.py
@@ -8,10 +8,20 @@ from .models import Game, Genre, Platform
 router = Router()
 
 # Game endpoints
-@router.get("/games", response=List[GameSchema])
-def list_games(request):
-    games = Game.objects.prefetch_related("genres", "platforms").all()
-    return games
+@router.get("/games/", response=list[GameSchema])
+def list_games(request, genre: str = None, platform: str = None):
+    """
+    Get all games, optionally filtered by genre or platform name.
+    """
+    qs = Game.objects.all()
+
+    if genre:
+        qs = qs.filter(genres__name__icontains=genre)
+
+    if platform:
+        qs = qs.filter(platforms__name__icontains=platform)
+
+    return qs
 
 @router.get("/games/{slug}", response=GameSchema)
 def get_game(request, slug: str):
