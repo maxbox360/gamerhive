b8ca9b4ab179d21a9756a4cfebea05b3203666e4
Max Sanderlin <msanderlin08@gmail.com>
2025-10-05 20:36:24 -0500

    Add schema and routers

--- backend/routers/games/router.py
+++ backend/routers/games/router.py
@@ -1,5 +1,29 @@
+# routers/games/router.py
+from django.shortcuts import get_object_or_404
 from ninja import Router
-from .models import Game
+from typing import List
+from .schema import GameSchema, GenreSchema, PlatformSchema
+from .models import Game, Genre, Platform
 
 router = Router()
 
+# Game endpoints
+@router.get("/games", response=List[GameSchema])
+def list_games(request):
+    games = Game.objects.prefetch_related("genres", "platforms").all()
+    return games
+
+@router.get("/games/{slug}", response=GameSchema)
+def get_game(request, slug: str):
+    game = get_object_or_404(Game.objects.prefetch_related("genres", "platforms"), slug=slug)
+    return game
+
+# Genre endpoints
+@router.get("/genres", response=List[GenreSchema])
+def list_genres(request):
+    return Genre.objects.all()
+
+# Platform endpoints
+@router.get("/platforms", response=List[PlatformSchema])
+def list_platforms(request):
+    return Platform.objects.all()
